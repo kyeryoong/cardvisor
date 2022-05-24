b17a33f957a157ba2f1bbd35ddf752fbf6a8f593
NeckyKim <kyeryoong@gmail.com>
2022-05-25 03:44:43 +0900

    brands logo was updated (220525)

--- src/components/service1/Service1.js
+++ src/components/service1/Service1.js
@@ -8,21 +8,22 @@ import { useNavigate } from 'react-router-dom';
 
 import SelectedBrands from './SelectedBrands';
 
+
 let jsonArr = [];
 
 
-function getJson(){
+function getJson() {
     let url = "http://localhost:8080/select";
 
     fetch(url, {
-        method:"post",
-        headers:{
-            "Content-Type":"application/json"
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
         },
-        body:JSON.stringify(jsonArr)
+        body: JSON.stringify(jsonArr)
     }).then(res => {
         console.log(res);
-    }).catch(err =>{
+    }).catch(err => {
         console.log(err);
     });
 }
@@ -45,95 +46,101 @@ function Card() {
 
 
 
-            <div>
-                <div className={styles.zone1}><button className={styles.categoryName} onClick={() => { setShow1(!show1); }}> 교통 </button></div>
-                <div className={styles.zone2}><button className={styles.hideShow} onClick={() => { setShow1(!show1); }}> {show1 ? '▲' : '▼'}</button></div>
-            </div>
 
-            <hr />
-            <div className={styles.row}>
-                {show1 && <Brands name="버스" brandlogo="transport_bus"/>}
-                {show1 && <Brands name="지하철" brandlogo="transport_subway" />}
-                {show1 && <Brands name="택시" brandlogo="transport_taxi" />}
-                {show1 && <Brands name="코레일" brandlogo="transport_korail" />}
-                {show1 && <Brands name="SRT" brandlogo="transport_srt" />}
-                {show1 && <Brands name="인천국제공항철도" brandlogo="transport_arex" />}
-            </div>
-            <br />
+            <details>
+                <summary className={styles.summaryZone}>
+                    <div className={styles.zone1}><div className={styles.categoryName} onClick={() => { setShow1(!show1); }}>교통</div></div>
+                    <div className={styles.zone2}><div className={styles.hideShow} onClick={() => { setShow1(!show1); }}>{show1 ? '▲' : '▼'}</div></div>
+                    <hr />
+                </summary>
 
-
-
-            <div>
-                <div className={styles.zone1}><button className={styles.categoryName} onClick={() => { setShow2(!show2); }}> 통신 </button></div>
-                <div className={styles.zone2}><button className={styles.hideShow} onClick={() => { setShow2(!show2); }}> {show2 ? '▲' : '▼'}</button></div>
-            </div>
-
-            <hr />
-            <div className={styles.row}>
-                {show2 && <Brands name="SK텔레콤" brandlogo="communication_skt" />}
-                {show2 && <Brands name="KT" brandlogo="communication_kt" />}
-                {show2 && <Brands name="LG유플러스" brandlogo="communication_lgu" />}
-                {show2 && <Brands name="KT olleh 인터넷" brandlogo="communication_kt" />}
-                {show2 && <Brands name="SKT 인터넷" brandlogo="communication_skt" />}
-                {show2 && <Brands name="LG U+ 인터넷" brandlogo="communication_lgu" />}
-                {show2 && <Brands name="SKT IPTV" brandlogo="communication_skt" />}
-                {show2 && <Brands name="KT IP-TV" brandlogo="communication_kt" />}
-                {show2 && <Brands name="LG U+ IPTV" brandlogo="communication_lgu" />}
-                {show2 && <Brands name="SKT 인터넷전화" brandlogo="communication_skt" />}
-                {show2 && <Brands name="KT 인터넷전화" brandlogo="communication_kt" />}
-                {show2 && <Brands name="LG U+ 인터넷전화" brandlogo="communication_lgu" />}
-            </div>
-            <br />
+                <div className={styles.row}>
+                    <Brands name="버스" brandlogo="transport_bus" />
+                    <Brands name="지하철" brandlogo="transport_subway" />
+                    <Brands name="택시" brandlogo="transport_taxi" />
+                    <Brands name="코레일" brandlogo="transport_korail" />
+                    <Brands name="SRT" brandlogo="transport_srt" />
+                    <Brands name="인천국제공항철도" brandlogo="transport_arex" />
+                </div>
+            </details>
 
 
+            <details>
+                <summary className={styles.summaryZone}>
+                    <div className={styles.zone1}><div className={styles.categoryName} onClick={() => { setShow2(!show2); }}>통신</div></div>
+                    <div className={styles.zone2}><div className={styles.hideShow} onClick={() => { setShow2(!show2); }}>{show2 ? '▲' : '▼'}</div></div>
+                    <hr />
+                </summary>
 
-            <div>
-                <div className={styles.zone1}><button className={styles.categoryName} onClick={() => { setShow3(!show3); }}> 마트 </button></div>
-                <div className={styles.zone2}><button className={styles.hideShow} onClick={() => { setShow3(!show3); }}> {show3 ? '▲' : '▼'}</button></div>
-            </div>
-
-            <hr />
-            <div className={styles.row}>
-                {show3 && <Brands name="이마트" brandlogo="mart_emart" />}
-                {show3 && <Brands name="홈플러스" brandlogo="mart_homeplus" />}
-                {show3 && <Brands name="롯데마트" brandlogo="mart_lottemart" />}
-                {show3 && <Brands name="농협하나로마트" brandlogo="mart_nhhanaro" />}
-                {show3 && <Brands name="이마트 트레이더스" brandlogo="mart_emarttraders" />}
-                {show3 && <Brands name="VIC 마켓" brandlogo="mart_vicmarket" />}
-                {show3 && <Brands name="하나로클럽" brandlogo="mart_hanaroclub" />}
-                {show3 && <Brands name="코스트코" brandlogo="mart_costco" />}
-                {show3 && <Brands name="홈플러스익스프레스" brandlogo="mart_homeplusexpress" />}
-                {show3 && <Brands name="이마트 에브리데이" brandlogo="mart_emarteveryday" />}
-                {show3 && <Brands name="GS마트" brandlogo="mart_gsmart" />}
-                {show3 && <Brands name="롯데슈퍼" brandlogo="mart_lottesuper" />}
-                {show3 && <Brands name="GS THE FRESH" brandlogo="mart_gsthefresh" />}
-            </div>
-            <br />
+                <div className={styles.row}>
+                    <Brands name="SK텔레콤" brandlogo="communication_skt" />
+                    <Brands name="KT" brandlogo="communication_kt" />
+                    <Brands name="LG유플러스" brandlogo="communication_lgu" />
+                    <Brands name="KT olleh 인터넷" brandlogo="communication_kt" />
+                    <Brands name="SKT 인터넷" brandlogo="communication_skt" />
+                    <Brands name="LG U+ 인터넷" brandlogo="communication_lgu" />
+                    <Brands name="SKT IPTV" brandlogo="communication_skt" />
+                    <Brands name="KT IP-TV" brandlogo="communication_kt" />
+                    <Brands name="LG U+ IPTV" brandlogo="communication_lgu" />
+                    <Brands name="SKT 인터넷전화" brandlogo="communication_skt" />
+                    <Brands name="KT 인터넷전화" brandlogo="communication_kt" />
+                    <Brands name="LG U+ 인터넷전화" brandlogo="communication_lgu" />
+                </div>
+            </details>
 
 
 
-            <div>
-                <div className={styles.zone1}><button className={styles.categoryName} onClick={() => { setShow4(!show4); }}> 편의점 </button></div>
-                <div className={styles.zone2}><button className={styles.hideShow} onClick={() => { setShow4(!show4); }}> {show4 ? '▲' : '▼'}</button></div>
-            </div>
+            <details>
+                <summary className={styles.summaryZone}>
+                    <div className={styles.zone1}><div className={styles.categoryName} onClick={() => { setShow3(!show3); }}>마트</div></div>
+                    <div className={styles.zone2}><div className={styles.hideShow} onClick={() => { setShow3(!show3); }}>{show3 ? '▲' : '▼'}</div></div>
+                    <hr />
+                </summary>
 
-            <hr />
-            <div className={styles.row}>
-                {show4 && <Brands name="GS25" brandlogo="convstore_gs25" />}
-                {show4 && <Brands name="CU" brandlogo="convstore_cu" />}
-                {show4 && <Brands name="세븐일레븐" brandlogo="convstore_seveneleven" />}
-                {show4 && <Brands name="미니스톱" brandlogo="convstore_ministop" />}
-                {show4 && <Brands name="이마트24" brandlogo="convstore_emart24" />}
-                {show4 && <Brands name="바이더웨이" brandlogo="convstore_buytheway" />}
-            </div>
-            <br /><br /><br />
+                <div className={styles.row}>
+                    <Brands name="이마트" brandlogo="mart_emart" />
+                    <Brands name="홈플러스" brandlogo="mart_homeplus" />
+                    <Brands name="롯데마트" brandlogo="mart_lottemart" />
+                    <Brands name="농협하나로마트" brandlogo="mart_nhhanaro" />
+                    <Brands name="이마트 트레이더스" brandlogo="mart_emarttraders" />
+                    <Brands name="VIC 마켓" brandlogo="mart_vicmarket" />
+                    <Brands name="하나로클럽" brandlogo="mart_hanaroclub" />
+                    <Brands name="코스트코" brandlogo="mart_costco" />
+                    <Brands name="홈플러스익스프레스" brandlogo="mart_homeplusexpress" />
+                    <Brands name="이마트 에브리데이" brandlogo="mart_emarteveryday" />
+                    <Brands name="GS마트" brandlogo="mart_gsmart" />
+                    <Brands name="롯데슈퍼" brandlogo="mart_lottesuper" />
+                    <Brands name="GS THE FRESH" brandlogo="mart_gsthefresh" />
+                </div>
+            </details>
+
+
+
+            <details>
+                <summary className={styles.summaryZone}>
+                    <div className={styles.zone1}><div className={styles.categoryName} onClick={() => { setShow4(!show4); }}>편의점</div></div>
+                    <div className={styles.zone2}><div className={styles.hideShow} onClick={() => { setShow4(!show4); }}>{show4 ? '▲' : '▼'}</div></div>
+                    <hr />
+                </summary>
+
+                <div className={styles.row}>
+                    <Brands name="GS25" brandlogo="convstore_gs25" />
+                    <Brands name="CU" brandlogo="convstore_cu" />
+                    <Brands name="세븐일레븐" brandlogo="convstore_seveneleven" />
+                    <Brands name="미니스톱" brandlogo="convstore_ministop" />
+                    <Brands name="이마트24" brandlogo="convstore_emart24" />
+                    <Brands name="바이더웨이" brandlogo="convstore_buytheway" />
+                </div>
+            </details>
+
+
 
             <div>
                 <Link to='/service1/results'>
                     <button className={styles.sendButton} onClick={() => {
-                    
-                        for(var i = 0; i < SelectedBrands.length; i++){
-                            jsonArr[i] = {"memberId" : 1, "brandName": SelectedBrands[i]}
+
+                        for (var i = 0; i < SelectedBrands.length; i++) {
+                            jsonArr[i] = { "memberId": 1, "brandName": SelectedBrands[i] }
                         }
                         getJson();
                     }}>
