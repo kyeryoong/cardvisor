5dc2efd1e81f72e49e44d649f39b223d32fc83f5
NeckyKim <kyeryoong@gmail.com>
2022-09-27 18:20:31 +0900

    card recommendation component was added on Main Page. (220927)

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,6 +7,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
+
 function Home() {
     const navigate = useNavigate();
 
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,33 @@
 import { useNavigate } from 'react-router';
+import Slider from 'react-slick';
 
 import styles from './Main.module.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
+function CardElements({number, name, comment}) {
+    const navigate = useNavigate();
+
+    return (
+        <div className={styles.cardElements}>
+            <div className={styles.cardElementsHeader}>
+                카드 추천
+            </div>
+
+            <div className={styles.cardElementsComment}>
+                {comment}
+            </div>
+
+            <div className={styles.cardElementsName}>
+                {name}
+            </div>
+
+            <img alt="card" className={styles.cardElementsImage} src={process.env.PUBLIC_URL + "/images/card_images/" + number + ".png"} />
+        </div>
+    )
+}
 
 
 
@@ -9,11 +36,30 @@ function Main() {
 
     return (
         <div>
-            <div className={styles.randomCardZone}>
-                <div className={styles.randomCardIntro}>
-                    오늘의 카드
-                </div>
+            <div className={styles.cardZone}>
+                <Slider {...{
+                    dots: false,
+                    arrows: false,
+                    infinite: true,
+                    fade: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    speed: 500,
+                    autoplaySpeed: 3000,
+                    pauseOnHover: false,
+                }}>
+                    <CardElements number={4073} name="현대카드M BOOST" comment="버스, 지하철, 택시 교통 혜택 제공" />
+
+                    <CardElements number={4070} name="현대카드Z Ontact" comment="넷플릭스, 왓챠, 유튜브 프리미엄 30% 할인" />
+
+                    <CardElements number={3511} name="가온올포인트 Check" comment="16개 카페 브랜드 포인트/마일리지 적립" />
+
+                    <CardElements number={3790} name="쇼핑앤조이카드" comment="온라인 쇼핑몰 5000원 할인" />
+                </Slider>
             </div>
+
+
 
             <div className={styles.buttonsZone}>
                 <div className={styles.button1} onClick={() => {
