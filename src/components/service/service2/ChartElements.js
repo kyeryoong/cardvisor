a73a923391762f72c335578cd4a3ee99c4e16bed
NeckyKim <kyeryoong@gmail.com>
2022-11-08 15:42:19 +0900

    layout was changed in Service2Analysis component. (221108)

--- src/components/service/service2/ChartElements.js
+++ src/components/service/service2/ChartElements.js
@@ -7,43 +7,29 @@ function ChartElements({ color, element, value, ratioByCategories, ratioByMaxVal
         &&
 
         <div className={styles.elementsContainer}>
-            <div className={styles.elementColor} style={{ backgroundColor: color }}>
+            <div className={styles.elementsContainer1}>
+                <div className={styles.elementColor} style={{ backgroundColor: color }}>
+                </div>
+
+                <div className={styles.elementName}>
+                    {element}
+                </div>
             </div>
 
-            <div className={styles.elementName}>
-                {element}
-            </div>
-
-            <div className={styles.elementValueContainer} style={{ color: color }}>
-                <div className={styles.elementValueContainerTop}>
+            <div className={styles.elementsContainer2}>
+                <div className={styles.elementValue} style={{ color: color }}>
                     {value}원
                 </div>
 
-                {/* <div className={styles.elementValueContainerBottom}>
-                    {num2}원
-                </div> */}
-            </div>
 
-            <div className={styles.elementBarContainer}>
-                <div className={styles.elementBarContainerTop}>
-                    <div className={styles.elementValue} style={{
-                        backgroundColor: color,
-                        width: (String(ratioByMaxValue * 200 + 100) + "px")
-                    }}>
-                        {(ratioByCategories * 100).toFixed(1)} %
-                    </div>
+                <div className={styles.elementBar} style={{
+                    backgroundColor: color,
+                    width: "calc(100px + " + (String(ratioByMaxValue * 20)) + "vw)"
+                }}>
+                    {(ratioByCategories * 100).toFixed(1)} %
                 </div>
-
-                {/* <div className={styles.elementBarContainerBottom}>
-                    <div className={styles.elementValue} style={{
-                        backgroundColor: color,
-                        width: (String(num1 * 200 + 150) + "px")
-                    }}>
-                        {(num1 * 100).toFixed(1)} %
-                    </div>
-                </div> */}
             </div>
-        </div>
+        </div >
     )
 }
 
